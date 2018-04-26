7096b89bedaca41581aea8703388c2d170205d37
Natalija Babkina <natalija.babkina@ne.linedata.com>
2018-04-26 12:06:28 +0300

    Empty solution

---
+++ AccountBalanceDomain/AccountBalanceDomain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AccountBalanceDomain
+{
+    public class Class1
+    {
+    }
+}
---
+++ AccountBalanceDomain/AccountBalanceTest/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AccountBalanceTest
+{
+    public class Class1
+    {
+    }
+}
